1de2d303faefeb6e86577ab9c4cf85d04cf99cea
Ricardo Oyarzun <royarzun@gmail.com>
2015-03-06 15:55:14 -0300

    Integrate apps and settings for ClaraWeb

--- ClaraWebREST/urls.py
+++ ClaraWebREST/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ClaraWebREST.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^FrontEnd/', include('FrontEnd.urls'))
 )

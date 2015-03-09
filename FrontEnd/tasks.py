65be5a9db47a22f00595b30d50041a9af2001d14
Ricardo Oyarzun <royarzun@gmail.com>
2015-03-09 15:31:13 -0300

    Erase the Upload example task

--- FrontEnd/tasks.py
+++ FrontEnd/tasks.py
@@ -4,21 +4,8 @@ Created on 06-03-2015
 @author: royarzun
 '''
 from ClaraWebREST import celery_app
-from xsys.xMsgFE import xMsgFE
+from src.sys.Dpe import Dpe
 from time import sleep
-
-@celery_app.task()
-def UploadTask(message):
-
-    # Update the state. The meta data is available in task.info dictionary
-    # The meta data is useful to store relevant information to the task
-    # Here we are storing the upload progress in the meta. 
-
-    UploadTask.update_state(state='PROGRESS', meta={'progress': 0})
-    sleep(30)
-    UploadTask.update_state(state='PROGRESS', meta={'progress': 30})
-    sleep(30)
-    return message
 
 
 def get_task_status(task_id):
@@ -41,7 +28,13 @@ def get_task_status(task_id):
 def StartNodeDPETask(message):
     print "FrontEnd about to be deployed"
     StartNodeDPETask.update_state(state='PROGRESS', meta={'progress': 0})
-    FE = xMsgFE()
+    try:
+        DPE_node = Dpe()
+        DPE_node.start()
+    except ValueError:
+        print "Oopsss..... Seems like the ip has been already taken"
+        pass
+        
     print "FrontEnd has been deployed"
     StartNodeDPETask.update_state(state='SUCCESS', meta={'progress': 100})
     sleep(100)

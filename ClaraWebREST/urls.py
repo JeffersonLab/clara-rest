66a64b9328f713b05d00bd1bd7689a1b07bd7e6e
Ricardo Oyarzun <royarzun@gmail.com>
2015-03-13 16:30:59 -0300

    Fix url ending issue at /frontends

--- ClaraWebREST/urls.py
+++ ClaraWebREST/urls.py
@@ -1,13 +1,17 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
 '''
-Created on 06-03-2015
 
+Created on 06-03-2015
 @author: royarzun
 '''
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from rest_framework.urlpatterns import format_suffix_patterns
+
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^FrontEnd/', include('FrontEnd.urls'))
+    url(r'^frontends/', include('FrontEnd.urls'))
 )
+
+urlpatterns = format_suffix_patterns(urlpatterns)

0300cfd8fb9f539cdb7afb340d838e439e64c394
Ricardo Oyarzun <royarzun@gmail.com>
2015-03-27 14:08:19 -0300

    Add Time zone exception for clearer testing

--- ClaraWebREST/settings.py
+++ ClaraWebREST/settings.py
@@ -85,7 +85,8 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
-USE_TZ = True
+# Set to false for development 
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)

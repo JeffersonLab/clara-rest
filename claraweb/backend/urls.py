8fa9ecea23896ca0f2b8075b3beb2ac39e51c793
Ricardo Oyarzun <royarzun@gmail.com>
2016-11-03 16:45:18 -0300

    backend: update urls, no default url after all this is an api

--- claraweb/backend/urls.py
+++ claraweb/backend/urls.py
@@ -2,16 +2,12 @@
 
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic.base import RedirectView
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = patterns('',
-                       url(r'^/?$', RedirectView.as_view(url='/dpes',
-                                                         permanent=True,
-                                                         query_string=False)),
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^api-auth/',
                            include('rest_framework.urls',

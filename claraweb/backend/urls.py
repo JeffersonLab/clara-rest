900c50232eb6b17cc2fe5b90ded8dbb2e194ae33
Ricardo Oyarzun <royarzun@gmail.com>
2016-11-03 17:34:25 -0300

    swagger: update main title

--- claraweb/backend/urls.py
+++ claraweb/backend/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='Pastebin API')
+schema_view = get_swagger_view(title='Clara API')
 
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),

5f885afc63433494890fdee5c0e0fa1a0e08ee59
Ricardo Oyarzun <royarzun@gmail.com>
2016-11-28 18:52:40 -0300

    Revert "swagger: downgrade django-rest-swagger version"
    This reverts commit 3f05f8376307b511a583d33168a4dbe0916abd9e.

--- claraweb/backend/urls.py
+++ claraweb/backend/urls.py
@@ -3,11 +3,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_swagger.views import get_swagger_view
 
+schema_view = get_swagger_view(title='Clara API')
 
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^docs/', include('rest_framework_swagger.urls')),
+                       url(r'^api-auth/',
+                           include('rest_framework.urls',
+                                   namespace='rest_framework')),
+                       url(r'^docs/', schema_view),
                        url(r'^dpes/',
                            include('backend.rest.DPE.urls')),
                        url(r'^containers/',

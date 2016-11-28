8491e82f5ee232f2866b5eed1aed6c78f50a1dc5
Ricardo Oyarzun <royarzun@gmail.com>
2016-11-28 08:59:44 -0300

    db: run migrations outside the container

--- development.sh
+++ development.sh
@@ -4,6 +4,10 @@ git clone https://github.com/JeffersonLab/xmsg_python.git ./claraweb/build/xmsg
 git clone https://github.com/JeffersonLab/clara-python.git ./claraweb/build/clara
 
 cd claraweb
+python manage.py makemigrations DPE --settings=settings.base
+python manage.py makemigrations Container --settings=settings.base
+python manage.py makemigrations Service --settings=settings.base
+python manage.py migrate --settings=settings.base --run-syncdb --no-input
 docker-compose -f ../scripts/docker-compose.dev.yml build
 docker-compose -f ../scripts/docker-compose.dev.yml up -d
 

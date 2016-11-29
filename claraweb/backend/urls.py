4db8d2203ee59260d8896812dc525df4a24e8035
Ricardo Oyarzun <royarzun@gmail.com>
2016-11-29 00:30:40 -0300

    Fix requirements

--- claraweb/backend/urls.py
+++ claraweb/backend/urls.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework_swagger.views import get_swagger_view
